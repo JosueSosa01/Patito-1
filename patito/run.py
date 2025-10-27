4da4acc93958542e495a8f3c890989e9874fdf84
JosueSosa01 <117098057+JosueSosa01@users.noreply.github.com>
2025-10-26 20:03:03 -0600

    Add files via upload

---
+++ patito/run.py
@@ -0,0 +1,14 @@
+from scanner import build_lexer
+from parser import build_parser
+
+def parse_text(src: str):
+    lexer = build_lexer()
+    parser = build_parser()
+    return parser.parse(src, lexer=lexer)
+
+if __name__ == "__main__":
+    import sys, io
+    src = open(sys.argv[1],encoding="utf-8").read() if len(sys.argv)>1 else sys.stdin.read()
+    ast = parse_text(src)
+    print("=== AST ===")
+    print(ast)
